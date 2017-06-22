b291585c66367d602de60732f26f1cd831af4a81
Bradford Bennett Jr <bbennettjr@gmail.com>
2017-06-22 08:30:34 -0400

    Change bootstrap -> react-bootstrap

--- imports/ui/components/header/header.jsx
+++ imports/ui/components/header/header.jsx
@@ -1,51 +1,32 @@
 import React from "react";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import NewBox from "./new_box/new_box.js";
 import AccountsWrapper from "../accounts/accounts_wrapper.js";
 
-export default class Header extends React.Component {
-	render() {
-		return (
-			<div className="container-fluid">
-				<nav className="navbar navbar-default">
-					<div className="container-fluid">
-						<div className="navbar-header">
-							<button
-								type="button"
-								className="navbar-toggle collapsed"
-								data-toggle="collapse"
-								data-target="#bs-example-navbar-collapse-1"
-								aria-expanded="false"
-							>
-								<span className="sr-only">
-									Toggle navigation
-								</span>
-								<span className="icon-bar" />
-								<span className="icon-bar" />
-								<span className="icon-bar" />
-							</button>
-							<a className="navbar-brand" href="/">Boxes</a>
-						</div>
-						<div
-							className="collapse navbar-collapse"
-							id="bs-example-navbar-collapse-1"
-						>
-							<ul className="nav navbar-nav">
-								<li className="active">
-									<a href="/home">Home</a>
-								</li>
-							</ul>
-							<ul className="nav navbar-nav navbar-right">
-								<li>
-									<NewBox />
-								</li>
-								<li>
-									<AccountsWrapper />
-								</li>
-							</ul>
-						</div>
-					</div>
-				</nav>
-			</div>
-		);
-	}
+// prettier-ignore
+export default Header = () => {
+	return (
+		<Navbar defaultExpanded>
+			<Navbar.Header>
+				<Navbar.Brand>
+					<a href="#">Boxes</a>
+				</Navbar.Brand>
+				<Navbar.Toggle />
+			</Navbar.Header>
+			<Navbar.Collapse>
+				<Nav>
+					<NavItem eventKey={1} href="#">Newest</NavItem>
+					<NavItem eventKey={2} href="#">Top</NavItem>
+				</Nav>
+				<Nav pullRight>
+					<NavItem eventKey={1} href="#">
+						<NewBox />
+					</NavItem>
+					<NavItem eventKey={2} href="#">
+						<AccountsWrapper />
+					</NavItem>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 }
